6782ba6ff77135d73ad82a6f993b0d88a2054ba2
Eugene <comradeeugene@gmail.com>
2022-02-05 16:11:30 +0300

    Добавьте файлы проекта.

---
+++ Lesson_8.1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Lesson_8._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> testList = new List<int>();
+            Random random = new Random();
+            for (int i = 0; i < 100; i++)
+            {
+                testList.Add(random.Next(101));
+            }
+            foreach (var e in testList)
+            {
+                Console.Write($"{e} ");
+            }
+            Console.WriteLine();
+            testList.RemoveAll(x => x >= 25 && x <= 50);
+            foreach (var e in testList)
+            {
+                Console.Write($"{e} ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
